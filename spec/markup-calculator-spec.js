0a6141ea21a722b4f4e01e3a2fbb8b9f16beef41
Jeff Johnson <energy.nexus@gmail.com>
2017-06-21 20:37:11 -0400

    Skeleton commit. Tests configured via npm.

---
+++ spec/markup-calculator-spec.js
@@ -0,0 +1,13 @@
+import MarkupCalculator from '../lib/markup-calculator.js'
+
+describe("Markup business logic", function(){
+  it("should solve for use case 1", function(){
+    var base_cost = 0;
+    var num_people = 0;
+    var category = undefined;
+
+    var calculator = null;
+
+  });
+});
+
