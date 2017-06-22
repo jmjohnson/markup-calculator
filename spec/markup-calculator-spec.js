f83d63487ad1ee08b05e39878b8d77a7b499c66d
Jeff Johnson <energy.nexus@gmail.com>
2017-06-21 22:27:59 -0400

    Stamped out the remaining two use cases.

--- spec/markup-calculator-spec.js
+++ spec/markup-calculator-spec.js
@@ -13,8 +13,25 @@ describe("Markup business logic", () => {
     let category = 'food';
 
     let final_price = calculator.calculatePrice(base_cost, num_people, category);
-    expect(final_price).toBe(1591.58)
+    expect(final_price).toBe(1591.58);
   });
 
+  it("should solve for use case 2.", () => {
+    let base_cost = 5432.00;
+    let num_people = 1;
+    let category = 'drugs';
+
+    let final_price = calculator.calculatePrice(base_cost, num_people, category);
+    expect(final_price).toBe(6199.81);
+  });
+
+  it("should solve for use case 3.", () => {
+    let base_cost = 12456.95;
+    let num_people = 4;
+    let category = 'books';
+
+    let final_price = calculator.calculatePrice(base_cost, num_people, category);
+    expect(final_price).toBe(13707.63);
+  });
 });
 
